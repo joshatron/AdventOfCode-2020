505d1bc06b3d395331656c1ed42ae7887ac8727b
Joshua Leger <legermjoshua@gmail.com>
2020-12-04 13:45:23 -0800

    Moved days vec to days module

--- src/days.rs
+++ src/days.rs
@@ -9,6 +9,15 @@ pub trait Day {
   fn puzzle_2(&self, input: &Vec<String>) -> String;
 }
 
+pub fn get_days() -> Vec<Box<dyn Day>> {
+  vec![
+    Box::new(day_1::Day1{}),
+    Box::new(day_2::Day2{}),
+    Box::new(day_3::Day3{}),
+    Box::new(day_4::Day4{}),
+  ]
+}
+
 pub fn input_to_ints(input: &Vec<String>) -> Vec<i64> {
   input.iter()
     .map(|l| l.parse::<i64>().unwrap())
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
 pub mod input;
 pub mod days;
 
-use days::Day;
-
 pub fn run() {
   println!("Advent of Code 2020 Results");
   println!("===========================");
 
-  let mut all_days: Vec<Box<dyn Day>> = Vec::new();
-  all_days.push(Box::new(days::day_1::Day1{}));
-  all_days.push(Box::new(days::day_2::Day2{}));
-  all_days.push(Box::new(days::day_3::Day3{}));
-  all_days.push(Box::new(days::day_4::Day4{}));
-
-  for day in all_days {
+  for day in days::get_days() {
     let day_input = input::get_day_input(day.day_num());
     println!("\nDay {}", day.day_num());
     println!("  Puzzle 1: {}", day.puzzle_1(&day_input));
