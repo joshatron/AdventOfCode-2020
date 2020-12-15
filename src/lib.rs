1e0fbe79967a0e8e1d01ce259d6484ad87b3fc5f
Joshua Leger <legermjoshua@gmail.com>
2020-12-15 13:53:38 -0800

    Added metrics for runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 pub mod input;
 pub mod days;
 
@@ -8,7 +10,13 @@ pub fn run() {
   for day in days::get_days() {
     let day_input = input::get_day_input(day.day_num());
     println!("\nDay {}", day.day_num());
-    println!("  Puzzle 1: {}", day.puzzle_1(&day_input));
-    println!("  Puzzle 2: {}", day.puzzle_2(&day_input));
+    let puzzle_1_start = Instant::now();
+    let puzzle_1 = day.puzzle_1(&day_input);
+    let puzzle_1_time = puzzle_1_start.elapsed().as_micros() as f64 / 1000.;
+    println!("  Puzzle 1 ({}ms): {}", puzzle_1_time, puzzle_1);
+    let puzzle_2_start = Instant::now();
+    let puzzle_2 = day.puzzle_2(&day_input);
+    let puzzle_2_time = puzzle_2_start.elapsed().as_micros() as f64 / 1000.;
+    println!("  Puzzle 2 ({}ms): {}", puzzle_2_time, puzzle_2);
   }
 }
