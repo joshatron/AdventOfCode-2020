258b888520d8d895eeca50a3a5c5f5bb4d0a0621
Joshua Leger <legermjoshua@gmail.com>
2020-12-15 12:20:08 -0800

    Drastically increased speed by using array instead of map

--- src/days/day_15.rs
+++ src/days/day_15.rs
@@ -1,7 +1,4 @@
 use crate::days::Day;
-use std::convert::TryInto;
-use std::collections::HashMap;
-use rustc_hash::FxHashMap;
 
 pub struct Day15 {}
 
@@ -38,37 +35,23 @@ fn parse_starting_sequence(line: &str) -> Vec<usize> {
     .collect()
 }
 
-fn num_since_last_occurance(nums: &Vec<usize>) -> usize {
-  let num_to_check = &nums[nums.len() - 1];
-
-  for (i, num) in nums.iter().rev().enumerate() {
-    if i > 0 && num == num_to_check {
-      return i;
-    }
-  }
-
-  0
-}
-
 struct Sequence {
-  turn: u64,
-  last: u64,
-  done: FxHashMap<u64, u64>,
+  turn: usize,
+  last: usize,
+  done: Vec<usize>,
 }
 
 impl Sequence {
   fn play_one_round(&mut self) {
-    match self.done.get(&self.last) {
-      Some(turn) => {
-        let new_last = self.turn - turn;
-        self.done.insert(self.last, self.turn);
+    if self.done[self.last] != 0 {
+        let new_last = self.turn - self.done[self.last];
+        self.done[self.last] = self.turn;
         self.last = new_last;
-      },
-      None => {
-        self.done.insert(self.last, self.turn);
+    } else {
+        self.done[self.last] = self.turn;
         self.last = 0;
-      }
     }
+
     self.turn += 1;
   }
 }
@@ -77,7 +60,7 @@ fn parse_to_sequence(line: &str) -> Sequence {
   let mut sequence = Sequence {
     turn: 0,
     last: 0, 
-    done: FxHashMap::default(),
+    done: vec![0; 30000000],
   };
 
   let mut s = parse_starting_sequence(line);
@@ -85,11 +68,11 @@ fn parse_to_sequence(line: &str) -> Sequence {
 
   for n in s {
     sequence.turn += 1;
-    sequence.done.insert(n.try_into().unwrap(), sequence.turn);
+    sequence.done[n] = sequence.turn;
   }
 
   sequence.turn += 1;
-  sequence.last = last.try_into().unwrap();
+  sequence.last = last;
 
   sequence
 }
@@ -105,29 +88,10 @@ mod tests {
   }
 
   #[test]
-  fn test_num_since_last_occurance() {
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6]), 0);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0]), 3);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0, 3]), 3);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0, 3, 3]), 1);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0, 3, 3, 1]), 0);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0, 3, 3, 1, 0]), 4);
-    assert_eq!(num_since_last_occurance(&vec![0, 3, 6, 0, 3, 3, 1, 0, 4]), 0);
-  }
-
-  #[test]
-  fn test_puzzle_1() {
-    assert_eq!(Day15{}.puzzle_1(&vec![String::from("0,3,6")]), "436");
-  }
-
-  #[test]
   fn test_parse_to_sequence() {
     let sequence = parse_to_sequence("0,3,6");
     assert_eq!(sequence.turn, 3);
     assert_eq!(sequence.last, 6);
-    assert_eq!(sequence.done.len(), 2);
-    assert_eq!(sequence.done.get(&0).unwrap(), &1);
-    assert_eq!(sequence.done.get(&3).unwrap(), &2);
   }
 
   #[test]
@@ -136,8 +100,6 @@ mod tests {
     sequence.play_one_round();
     assert_eq!(sequence.turn, 4);
     assert_eq!(sequence.last, 0);
-    assert_eq!(sequence.done.len(), 3);
-    assert_eq!(sequence.done.get(&6).unwrap(), &3);
     sequence.play_one_round();
     assert_eq!(sequence.turn, 5);
     assert_eq!(sequence.last, 3);
@@ -150,7 +112,7 @@ mod tests {
   }
 
   #[test]
-  fn test_puzzle_2() {
-    assert_eq!(Day15{}.puzzle_2(&vec![String::from("0,3,6")]), "175594");
+  fn test_puzzle_1() {
+    assert_eq!(Day15{}.puzzle_1(&vec![String::from("0,3,6")]), "436");
   }
 }
